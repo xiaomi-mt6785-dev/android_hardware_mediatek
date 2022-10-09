60c7913f3c78e6f25365a8eafdda9e49a534d352
bengris32 <bengris32@protonmail.ch>
2022-10-09 20:09:58 +0100

    power-mediatek: Simplify makefile * We can finally get rid of the ugly TARGET_USES_MTK_POWERHAL   as we don't have any dependencies except those present in AOSP.
    * We also don't need to specify TARGET_POWERHAL_AIDL_VERSION anymore,
      as we can simply build either android.hardware.power-service-V2-mediatek
      or android.hardware.power-service-V1-mediatek and so on.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- aidl/power-mediatek/Android.mk
+++ aidl/power-mediatek/Android.mk
@@ -1,24 +1,16 @@
-ifeq ($(TARGET_USES_MTK_POWERHAL),true)
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-# Default to the latest AIDL version
-TARGET_POWERHAL_AIDL_VERSION ?= 2
-
-LOCAL_MODULE := android.hardware.power-service-mediatek
+LOCAL_MODULE := android.hardware.power-service-V1-mediatek
 LOCAL_VENDOR_MODULE := true
 LOCAL_MULTILIB := 64
-LOCAL_VINTF_FRAGMENTS := power-mtk-v$(TARGET_POWERHAL_AIDL_VERSION).xml
+LOCAL_VINTF_FRAGMENTS := power-mtk-v1.xml
 LOCAL_SRC_FILES := Power.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     libbase \
     libbinder_ndk \
-    android.hardware.power-V$(TARGET_POWERHAL_AIDL_VERSION)-ndk
-
-LOCAL_CFLAGS += -DPOWERHAL_AIDL_VERSION=$(TARGET_POWERHAL_AIDL_VERSION)
+    android.hardware.power-V1-ndk
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
@@ -29,6 +21,39 @@ ifneq ($(TARGET_POWERHAL_MODE_EXT),)
     LOCAL_SRC_FILES += ../../../$(TARGET_POWERHAL_MODE_EXT)
 endif
 
+LOCAL_CFLAGS += -DPOWERHAL_AIDL_VERSION=1
+
+# This is hacky but I havent found another way that works.
+LOCAL_POST_INSTALL_CMD := mv $(TARGET_OUT_VENDOR)/lib64/$(LOCAL_MODULE).so \
+    $(TARGET_OUT_VENDOR)/lib64/android.hardware.power-service-mediatek.so
+
 include $(BUILD_SHARED_LIBRARY)
 
-endif # TARGET_USES_MTK_POWERHAL
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.power-service-V2-mediatek
+LOCAL_VENDOR_MODULE := true
+LOCAL_MULTILIB := 64
+LOCAL_VINTF_FRAGMENTS := power-mtk-v2.xml
+LOCAL_SRC_FILES := Power.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libbase \
+    libbinder_ndk \
+    android.hardware.power-V2-ndk
+
+ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
+    LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+endif
+
+ifneq ($(TARGET_POWERHAL_MODE_EXT),)
+    LOCAL_CFLAGS += -DMODE_EXT
+    LOCAL_SRC_FILES += ../../../$(TARGET_POWERHAL_MODE_EXT)
+endif
+
+LOCAL_CFLAGS += -DPOWERHAL_AIDL_VERSION=2
+
+# This is hacky but I havent found another way that works.
+LOCAL_POST_INSTALL_CMD := mv $(TARGET_OUT_VENDOR)/lib64/$(LOCAL_MODULE).so \
+    $(TARGET_OUT_VENDOR)/lib64/android.hardware.power-service-mediatek.so
+
+include $(BUILD_SHARED_LIBRARY)
